18c94d3011ac6d51f52fac5245c0b800cd105777
diego-guisosi <diego.guisosi@gmail.com>
2021-05-06 22:19:23 -0300

    add FairBatchBuilderTest

---
+++ src/test/java/br/com/dixy/tdd/practice/batch_builder/FairBatchBuilderTest.java
@@ -0,0 +1,24 @@
+package br.com.dixy.tdd.practice.batch_builder;
+
+/**
+ * Given a list of elements of different types, build a batch of N elements containing a fair amount of
+ * elements by type
+ * <p>
+ * e.g:
+ * Providing a list -> [
+ * TypeA:ElementA1, TypeA:ElementA2, TypeA:ElementA3, TypeA:ElementA4,
+ * TypeB:ElementB1, TypeB:ElementB2, TypeB:ElementB3, TypeB:ElementB4,
+ * TypeC:ElementC1, TypeC:ElementC2, TypeC:ElementC3, TypeC:ElementC4
+ * ]
+ * <p>
+ * Providing a batch size of 6
+ * <p>
+ * Returns a list -> [
+ * TypeA:ElementA1, TypeA:ElementA2,
+ * TypeB:ElementB1, TypeB:ElementB2,
+ * TypeC:ElementC1, TypeC:ElementC2
+ * ]
+ */
+public class FairBatchBuilderTest {
+
+}
