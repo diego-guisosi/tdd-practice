b839de54a1b0e8ca3a7465f0a86bc4c686fd6da6
diego-guisosi <diego.guisosi@gmail.com>
2021-05-06 23:09:02 -0300

    builds single element batch when list has one element and batch size is greater than zero

--- src/test/java/br/com/dixy/tdd/practice/batch_builder/FairBatchBuilderTest.java
+++ src/test/java/br/com/dixy/tdd/practice/batch_builder/FairBatchBuilderTest.java
@@ -2,10 +2,10 @@ package br.com.dixy.tdd.practice.batch_builder;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -29,21 +29,41 @@ import static org.junit.Assert.assertTrue;
  */
 public class FairBatchBuilderTest {
 
+    private static final TypedElement A1 = new TypedElement(Type.A, "A1");
+
     @Test
     public void buildsEmptyBatchWhenListIsEmpty() {
         int batchSize = 1;
         FairBatchBuilder builder = new FairBatchBuilder(batchSize);
         List<TypedElement> batch = builder.build(Collections.emptyList());
-        assertTrue(batch.isEmpty());
+        assertEmpty(batch);
     }
 
     @Test
     public void buildsEmptyBatchWhenBatchSizeIsZero() {
         int batchSize = 0;
         FairBatchBuilder builder = new FairBatchBuilder(batchSize);
-        List<TypedElement> list = Collections.singletonList(new TypedElement(Type.A, "A1"));
+        List<TypedElement> list = Collections.singletonList(A1);
         List<TypedElement> batch = builder.build(list);
-        assertTrue(batch.isEmpty());
+        assertEmpty(batch);
+    }
+
+    @Test
+    public void buildsSingleElementBatchWhenListHasOneElementAndBatchSizeIsGreaterThanZero() {
+        int batchSize = 2;
+        FairBatchBuilder builder = new FairBatchBuilder(batchSize);
+        List<TypedElement> list = Collections.singletonList(A1);
+        List<TypedElement> batch = builder.build(list);
+        assertSize(batch, 1);
+        assertEquals("element", A1, batch.get(0));
+    }
+
+    private void assertEmpty(List<TypedElement> batch) {
+        assertTrue("batch", batch.isEmpty());
+    }
+
+    private void assertSize(List<TypedElement> batch, int expected) {
+        assertEquals("batchSize", expected, batch.size());
     }
 
 }
